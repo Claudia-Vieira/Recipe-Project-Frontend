4cfca932d79044193b7ffa2dd69795c6c7fff5d8
Claudia Vieira <clau_f.v@hotmail.com>
2022-01-09 12:26:07 +0000

    separar nav bar num novo componente

--- project-frontend/src/app/app.module.ts
+++ project-frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchFormComponent
+    SearchFormComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
