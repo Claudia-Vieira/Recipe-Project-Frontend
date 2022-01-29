6f6fd1924a625855e27df62db9e72f4f507e4814
Claudia Vieira <clau_f.v@hotmail.com>
2022-01-29 18:39:38 +0000

    Melhoramento do service, com criação do ng for para os cards e leitura da informação da DB para os cartões + introdução de um elemento na navbar para aceder a pagina das descrições

--- project-frontend/src/app/services/recipe.service.ts
+++ project-frontend/src/app/services/recipe.service.ts
@@ -1,20 +1,19 @@
-import { Injectable} from '@angular/core';
+import { Inject, Injectable} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Recipe } from '../model/recipes';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class RecipeService {
 
-  public baseUrl: 'https://localhost:44380/';
+  private baseUrl = 'https://localhost:44380/';
 
-  constructor(private http: HttpClient, baseUrl: any) {
-    this.baseUrl = baseUrl;
+  constructor(private http: HttpClient,) {
   }
 
   getRecipes(): Observable<Recipe[]> {
     return this.http.get<Recipe[]>(this.baseUrl + 'api/recipes');
   }
+
 }
