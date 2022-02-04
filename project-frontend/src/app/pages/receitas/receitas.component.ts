8e556983863f9faeb67219995343ba7451686545
Claudia Vieira <clau_f.v@hotmail.com>
2022-02-04 22:35:05 +0000

    novo botao adicionar

--- project-frontend/src/app/pages/receitas/receitas.component.ts
+++ project-frontend/src/app/pages/receitas/receitas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-receitas',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./receitas.component.css']
 })
 export class ReceitasComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  navigateToAddForm(){
+    this.router.navigateByUrl('adicionar');
+  }
 }
